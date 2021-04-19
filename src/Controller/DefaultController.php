468754ba3f88c828eed4cfe976c52c566e43eb46
Yannis <rezzoug.yannis@gmail.com>
2021-04-20 01:27:04 +0200

    test other controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 
 class DefaultController extends AbstractController
