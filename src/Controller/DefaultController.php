550dc219120f4d7f0261c7b083317a2cc28c2730
Yannis <rezzoug.yannis@gmail.com>
2021-04-20 01:25:46 +0200

    test other controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,12 +11,19 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class DefaultController extends AbstractController
 {
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/", name="index")
      */
@@ -27,6 +34,21 @@ class DefaultController extends AbstractController
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
+    }
+
+    /**
+     * @Route("/test/all-users", name="get_all_users", methods={"GET"})
+     * @Assert\Json(
+     *     message = "You've entered an invalid Json."
+     * )
+     */
+    public function getAllUsers(): JsonResponse
+    {
+        $users = $this->em->getRepository(User::class)->findAll();
+        return $this->json([
+            "status" => 200,
+            "data" => $users
+        ]);
     }
 
 }
